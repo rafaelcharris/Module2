74fb53f1e21c62e935e801cb6066ce21b1450de6
Rafael Charris <rafaelcharris92@gmail.com>
2021-11-11 16:31:10 -0800

    Add code for the map (not in the app)
    This is to explore leaflet functions before running the app

--- NJmap.R
+++ NJmap.R
@@ -9,13 +9,19 @@ nj_counties <- counties("NJ", year = 2020)
 # Merge data from the county with the main data set
 
 df_map <- df %>%
-    left_join(nj_counties, by = c("county" = "NAME")) %>%
-    mutate(lat = as.numeric(INTPTLAT),
-    long = as.numeric(INTPTLON))
+    left_join(nj_counties, by = c("county" = "NAME"))
 
 # Draft the first map
 
 nj_counties %>%
-leaflet() %>%
-addTiles() %>%
-addPolygons(lng = long, lat = lat)
+  leaflet() %>%
+  addTiles() %>%
+  addPolygons(fillColor = "grey", 
+              color = "black", #Line Color
+              weight = 1.5, # Line thickness
+              label = nj_counties$NAME, # Add labels when hovering over the territory
+              smoothFactor = 0.2
+  )  %>%
+  addLegend(
+    colors = number.to.colors(df_map$pctblack_adjpop, colors = c("red", "blue"), num = 100),
+    values = df_map$pctblack_adjpop)
