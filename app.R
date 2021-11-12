40ce2454945064d064a25ed9ed944fac88696c6c
SteveVu2212 <83741887+SteveVu2212@users.noreply.github.com>
2021-11-12 00:29:01 -0800

    Update app.R
    add charts

--- app.R
+++ app.R
@@ -12,6 +12,8 @@ df <- read.csv(".//data//data.csv") %>%
   separate(town_county, sep = ",", c("city", "county_name")) %>% 
   mutate(city = tolower(as.character(coverage_city)),
          county = tolower(county))
+df <- head(df, -1)
+df <- df %>% drop_na()
 
 ##2.2 Load Geographical Data 
 nj_counties <- counties("NJ",
@@ -31,6 +33,38 @@ nj_mun <- read.csv(".//data//MunicipalData.csv", skip = 1) %>%
 # 4.1 Match the population of the city with the full data set
 df_city_pop <- inner_join(df, nj_mun, by = c("county", "city"))
 
+# 5. Load county-level data
+url = 'https://raw.githubusercontent.com/rafaelcharris/Module2/master/data/county_level_df.csv'
+df_cl = read.csv(url)
+
+## 5.1 Data for a bar chart showing incidents from 2012 to 2016
+
+df1 <- df[c('county', 'total_incidents_2012', 'total_incidents_2013', 'total_incidents_2014','total_incidents_2015','total_incidents_2016')]
+df1 <-  df1 %>%
+  group_by(county) %>%
+  summarise(across(everything(), mean))
+colnames(df1) <- c('county', 2012:2016)
+
+df1.long = melt(df1,id.vars = 'county', variable.name = "time", value.name = "incidents")
+df1.long$county = tolower(df1.long$county)
+
+## 5.2 Data for a bar chart showing percentage of ethnicity officers and subjects
+
+df_ofc <- df_cl %>%
+  dplyr::select(county, contains("per_police")) %>%
+  pivot_longer(cols = matches("per_police"),
+               names_to = "per_officers")
+
+names(df_ofc)[3] <- 'per_ofc'
+
+df_sub <- df_cl %>%
+  dplyr::select(county, contains('per_subject')) %>%
+  pivot_longer(cols = matches("per_subject"),
+               names_to = "per_subjects")
+
+df_sub <- df_sub[!grepl("injured", df_sub$per_subjects),]
+names(df_sub)[3] <- 'per_sub'
+
 # Define UI for application that draws a histogram
 ui <- fluidPage(
   
@@ -42,6 +76,18 @@ ui <- fluidPage(
   ),
   # Show a plot of the generated distribution
   fluidRow(
+  )
+
+  sidebarLayout(
+  sidebarPanel(uiOutput("countyOutput")),
+
+  mainPanel(plotOutput('inc_bar'),
+            br(),
+            plotOutput('eth_ofc_bar'),
+            br(),
+            plotOutput('eth_sub_bar'),
+            br(),
+            plotOutput("radar_forces"))
   )
 )
 
@@ -59,8 +105,45 @@ server <- function(input, output) {
                   weight = 1.5, # Line thickness
                   label = nj_counties$NAME, # Add labels when hovering over the territory
                   smoothFactor = 0.2
-                  )  
+                  )
     })
+  filtered <- reactive(df1.long %>% filter(county %in% input$countyInput))
+  filtered_1 <- reactive(df_ofc %>% filter(county %in% input$countyInput))
+  filtered_2 <- reactive(df_sub %>% filter(county %in% input$countyInput))
+  
+  output$inc_bar <- renderPlot({
+    if(is.null(input$countyInput)){
+      return (NULL)
+    }
+    if (length(input$countyInput) == 0) return ()
+    
+    ggplot(filtered()) +
+      geom_bar(mapping = aes(x = time,y=incidents), fill='blue', stat = 'identity')})
+
+  
+  output$eth_ofc_bar <- renderPlot({
+  if(is.null(input$countyInput)){
+    return (NULL)
+    }
+  if (length(input$countyInput) == 0) return ()
+
+  ggplot() +
+    geom_bar(mapping = aes(x = filtered_1()$per_officers,y=filtered_1()$per_ofc), fill='blue', stat = 'identity')})
+  
+  output$eth_sub_bar <- renderPlot({
+    if(is.null(input$countyInput)){
+      return (NULL)
+    }
+    if (length(input$countyInput) == 0) return ()
+    
+    ggplot(filtered_2()) +
+      geom_bar(mapping = aes(x = per_subjects,y=per_sub), fill='red', stat = 'identity')})
+  
+  output$countyOutput <- renderUI({
+    selectInput('countyInput', 'County',
+                choices = sort(unique(df_ofc$county)),
+                selected = 'Atlantic')
+  
 }
 
 # Run the application 
